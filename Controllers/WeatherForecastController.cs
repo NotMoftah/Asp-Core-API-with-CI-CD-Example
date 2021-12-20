85fc0ab9098c2a86c917cffc92a423d3a888eb31
Raaed Serag <raaedserag@gmail.com>
2021-12-20 22:40:40 +0200

    Test commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,10 +23,9 @@ namespace SlsApi.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                // Date = DateTime.Now.AddDays(index),
-                // TemperatureC = Random.Shared.Next(-20, 55),
-                // Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-                Summary = "Raaed"
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
